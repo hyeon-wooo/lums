d5e61490581e473384f7c90a771379cbdeeef070
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-08-28 16:18:03 +0900

    fix: 그룹 멤버 변경 후 변경사항 반영

--- src/group/command/add_member.js
+++ src/group/command/add_member.js
@@ -3,6 +3,8 @@ import execCommand from "../../shared/exec_command.js";
 const addMember = async ({ groupName, userName }) => {
   const command = `gpasswd -a ${userName} ${groupName}`;
   execCommand(command);
+
+  execCommand(`newgrp ${groupName}`);
 };
 
 export default addMember;
--- src/group/command/del_member.js
+++ src/group/command/del_member.js
@@ -3,6 +3,8 @@ import execCommand from "../../shared/exec_command.js";
 const delMember = async ({ groupName, userName }) => {
   const command = `gpasswd -d ${userName} ${groupName}`;
   execCommand(command);
+
+  execCommand(`newgrp ${groupName}`);
 };
 
 export default delMember;
